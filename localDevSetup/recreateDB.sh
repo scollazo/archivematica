47c82b804eb6bb7afee1190763d5cc2a2396f30c
Holly Becker <hbecker@artefactual.com>
2016-02-10 10:08:35 -0800

    Migrations: Data migrations for 1.4.1 to 1.5

--- localDevSetup/recreateDB.sh
+++ localDevSetup/recreateDB.sh
@@ -57,7 +57,7 @@ echo "Creating MCP database"
 mysql -u root "${dbpassword}" --execute="CREATE DATABASE ${databaseName} CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
 echo "Creating ${username} user"
 mysql -u root "${dbpassword}" --execute="CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}';"
-mysql -u root "${dbpassword}" --execute="GRANT SELECT, UPDATE, INSERT, DELETE, CREATE, ALTER, INDEX ON ${databaseName}.* TO '${username}'@'localhost';"
+mysql -u root "${dbpassword}" --execute="GRANT SELECT, UPDATE, INSERT, DELETE, CREATE, ALTER, INDEX, DROP ON ${databaseName}.* TO '${username}'@'localhost';"
 
 echo "Creating and populating MCP tables"
 
